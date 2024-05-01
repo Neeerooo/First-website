dab4e04bf090fe443f8d61481cdabc80dddc016f
Nero24 <153308025+Neeerooo@users.noreply.github.com>
2024-05-01 21:30:40 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+//const nama = ('ofren dialsa');
+//let tahunLahir = 1994;
+//let tahunsekarang = 2023;
+//const umur = tahunsekarang - tahunLahir;
+
+//let bioData = document.getElementById('biodata');
+
+//function BioData() {
+    //let generasi;
+    //if(umur < 7 && umur > 3) {
+        //console.log("anda adalah balita")
+        //generasi = "alpha";
+    //} else if(umur < 13 && umur > 7) {
+        //console.log('anda adalah anak-anak')
+        //generasi = "beta";
+    //} else if(umur < 18 && umur > 13) {
+        //console.log('anda adalah anak-anak')
+        //generasi = "X";
+    //} else if(umur < 120 && umur > 18) {
+        //console.log('anda adalah orang dewasa')
+        //generasi ="sepuh";
+    //} else {
+        //console.log("anda alien");
+    //}
+
+    //return bioData.innerHTML= generasi;
+//}
+
+//console.log(`nama saya adalah ${nama} dan usia saya adalah ${umur}`);
+//BioData();
+
+<button type="button" onclick="document.getElementById('demo').innerHTML = 'Hello JavaScript!'">Click Me!</button>
